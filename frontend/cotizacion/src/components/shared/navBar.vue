5ad5dead3411800fa12688740029aabdaa4e2ef6
Alan Beck <beckalan303@gmail.com>
2020-11-15 12:16:17 -0300

    add a heroku deploys

--- frontend/cotizacion/src/components/shared/navBar.vue
+++ frontend/cotizacion/src/components/shared/navBar.vue
@@ -10,7 +10,7 @@
                 <!-- Right aligned nav items -->
                 <b-navbar-nav class="ml-auto">
                     <b-nav-form>
-                        <b-form-input size="sm" class="mr-sm-2" v-model="text" placeholder="Ingresar Moneda"></b-form-input>
+                        <b-form-input size="sm" class="mr-sm-2" v-model="text" @keypress.enter="searchText" placeholder="Ingresar Moneda"></b-form-input>
                         <b-button size="sm" class="my-2 my-sm-0" type="button" @click="searchText">Buscar</b-button>
                     </b-nav-form>
                    
